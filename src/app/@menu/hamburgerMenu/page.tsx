74aa4df01d61563160b76f95d114739d7c0f73cb
Amir Ali <amiraliazimloo123@gmail.com>
2024-06-29 00:56:44 -0700

    Remove unused codes.

--- src/app/@menu/hamburgerMenu/page.tsx
+++ src/app/@menu/hamburgerMenu/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import Button from "@/components/Button";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 const HamburgerMenu = () => {
-  const router = useRouter();
   return (
     <>
         <div className="absolute top-0 bottom-0 left-0 right-50">
